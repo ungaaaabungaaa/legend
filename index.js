4e46c508a7ef836114fd443001127c2d57ca0321
Sydney Cohen <chnsydney@gmail.com>
2021-07-22 10:27:33 +0200

    refactor main

--- index.js
+++ index.js
@@ -1,42 +1,45 @@
-const Twitter = require('twitter-lite');
+const Twitter = require("twitter-lite");
 const Twitterclient = new Twitter({
-    subdomain: "api",
-    version: "1.1",
-    consumer_key: process.env.LEGEND_API_KEY_TWITTER,
-    consumer_secret: process.env.LEGEND_API_SECRET_KEY_TWITTER,
-    access_token_key: process.env.LEGEND_ACCESS_TOKEN_TWITTER,
-    access_token_secret: process.env.LEGEND_ACCESS_TOKEN_SECRET_TWITTER,
-})
+  subdomain: "api",
+  version: "1.1",
+  consumer_key: process.env.LEGEND_API_KEY_TWITTER,
+  consumer_secret: process.env.LEGEND_API_SECRET_KEY_TWITTER,
+  access_token_key: process.env.LEGEND_ACCESS_TOKEN_TWITTER,
+  access_token_secret: process.env.LEGEND_ACCESS_TOKEN_SECRET_TWITTER,
+});
 
-const owners = require('./repo.json');
-const { listPullRequests } = require('./utils');
-const { createTwitterMessage } = require('./composer');
-const { blackList } = require('./blackList');
+const owners = require("./repo.json");
+const { listPullRequests } = require("./utils");
+const { createTwitterMessage } = require("./composer");
+const { blackList } = require("./blackList");
 
-async function main() {
-    for (const owner in owners) {
-        const repo = owners[owner].repo
-        const repoTwitter = owners[owner].twitter
-        const prList = await listPullRequests({owner, repo })
-        for (const pr of prList) {
-            const { user: {twitter: twitterHandle}} = pr;            
-            if (blackList.includes(twitterHandle.toLowerCase())) {
-                continue
-            }
-            const status = createTwitterMessage({...pr, repo, repoTwitter })
-            console.log("🐦", status)
-            let resp;
-            // resp = await Twitterclient.post("statuses/update", {
-            //     status,
-            //   });
-            if (!resp?.created_at) {
-                console.log("🔴 error tweeting:'", status, "'")
-            }
-
-        }
+async function tweetToPrList(prList) {
+  for (const pr of prList) {
+    const {
+      user: { twitter: twitterHandle },
+    } = pr;
+    if (blackList.includes(twitterHandle.toLowerCase())) {
+      continue;
     }
-
+    const status = createTwitterMessage({ ...pr, repo, repoTwitter });
+    console.log("🐦", status);
+    let resp;
+    // resp = await Twitterclient.post("statuses/update", {
+    //     status,
+    //   });
+    if (!resp?.created_at) {
+      console.log("🔴 error tweeting:'", status, "'");
+    }
+  }
 }
 
-main()
+async function main() {
+  for (const owner in owners) {
+    const repo = owners[owner].repo;
+    const repoTwitter = owners[owner].twitter;
+    const prList = await listPullRequests({ owner, repo });
+    tweetToPrList(prList);
+  }
+}
 
+main();
