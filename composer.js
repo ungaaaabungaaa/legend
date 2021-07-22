dadfa03bd8423aa702528cb1b632c75ac0045e83
Arthur Coudouy <arthur.coudouy@gmail.com>
2021-07-22 10:31:19 +0200

    saving for merge current version

--- composer.js
+++ composer.js
@@ -10,6 +10,7 @@ function createTwitterMessage(params) {
     const { user: { twitter }, prTitle, repo, html_url: htmlUrl } = params;
 
     let prTitle2 = prTitle.length > 15 ? prTitle.slice(0, 15) + '...' : prTitle;
+    const repo2 = repo.replace('https://', '')
 
     const greetingsEmoji =
         greetingsEmojiAll[Math.floor(Math.random() * greetingsEmojiAll.length)];
@@ -17,7 +18,7 @@ function createTwitterMessage(params) {
 
     let message = `${greetings} @${twitter} ${greetingsEmoji}, `;
 
-    message += `your pull request "${prTitle2}" has been merged on ${repo}! `;
+    message += `your pull request "${prTitle2}" has been merged on ${repo2}! `;
     const conclusion = conclusionAll[Math.floor(Math.random() * conclusionAll.length)] + ' ' + htmlUrl + `.`;
 
     message += conclusion;
--- index.js
+++ index.js
@@ -3,41 +3,41 @@ const { listPullRequests } = require('./utils');
 const { createTwitterMessage } = require('./composer')
 
 const prList = [
-    {
-      prTitle: 'chore(i18n,curriculum): processed translations',
-      html_url: 'https://github.com/freeCodeCamp/freeCodeCamp/pull/42957',
-      merged_at: '2021-07-21T15:23:20Z',
-      labels: 'crowdin-sync,scope: curriculum,scope: i18n',
-      user: {
-        twitter: 'freeCodeCamp',
-        login: 'camperbot',
-        name: 'camperbot',
-        avatar: 'https://avatars.githubusercontent.com/u/13561988?v=4'
-      }
-    },
-    {
-      prTitle: 'chore(i18n,curriculum): processed translations',
-      html_url: 'https://github.com/freeCodeCamp/freeCodeCamp/pull/42943',
-      merged_at: '2021-07-20T16:05:24Z',
-      labels: 'crowdin-sync,scope: curriculum,scope: i18n',
-      user: {
-        twitter: 'freeCodeCamp',
-        login: 'camperbot',
-        name: 'camperbot',
-        avatar: 'https://avatars.githubusercontent.com/u/13561988?v=4'
-      }
+  {
+    prTitle: 'chore(i18n,curriculum): processed translations',
+    html_url: 'https://github.com/freeCodeCamp/freeCodeCamp/pull/42957',
+    merged_at: '2021-07-21T15:23:20Z',
+    labels: 'crowdin-sync,scope: curriculum,scope: i18n',
+    user: {
+      twitter: 'freeCodeCamp',
+      login: 'camperbot',
+      name: 'camperbot',
+      avatar: 'https://avatars.githubusercontent.com/u/13561988?v=4'
     }
-  ]
+  },
+  {
+    prTitle: 'chore(i18n,curriculum): processed translations',
+    html_url: 'https://github.com/freeCodeCamp/freeCodeCamp/pull/42943',
+    merged_at: '2021-07-20T16:05:24Z',
+    labels: 'crowdin-sync,scope: curriculum,scope: i18n',
+    user: {
+      twitter: 'freeCodeCamp',
+      login: 'camperbot',
+      name: 'camperbot',
+      avatar: 'https://avatars.githubusercontent.com/u/13561988?v=4'
+    }
+  }
+]
 
 async function main() {
-    for (const owner in owners) {
-        const prList = await listPullRequests({owner, repo: owners[owner]})
-        for (const pr of prList) {
-            const message = createTwitterMessage({...pr, repo: owners[owner]})
-            console.log("🐦", message)
-            // TODO tweet message
-        }
+  for (const owner in owners) {
+    const prList = await listPullRequests({ owner, repo: owners[owner] })
+    for (const pr of prList) {
+      const message = createTwitterMessage({ ...pr, repo: owners[owner] })
+      console.log("🐦", message)
+      // TODO tweet message
     }
+  }
 
 }
 
